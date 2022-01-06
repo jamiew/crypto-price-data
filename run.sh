554461fa6c8c01c88a34e5283e6ddbcb0b670e89
Jamie Wilkinson <jamie@jamiedubs.com>
2022-01-05 22:21:58 -0500

    Make run.sh more cron-friendly

--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+cd "$(dirname "$0")"
+
 ./coingecko-symbols.sh > data/symbols.json
 ./coingecko-prices.sh > data/prices.json
 node price-magic.js > data/usd.json
-cat data/usd.json | jq
+# cat data/usd.json | jq
+cat data/usd.json
 
