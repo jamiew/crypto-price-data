957ebcbae6a6f8e585b1267909362dd6824f7a51
Jamie Wilkinson <jamie@jamiedubs.com>
2024-01-04 12:01:10 -0500

    Include my gist-saving script. If you want to re-use this, be my guest :)

---
+++ save-to-gist.sh
@@ -0,0 +1,20 @@
+# web URL:
+# https://gist.github.com/jamiew/92937fc7b2a3e0192190b10c1cc8fa6b
+# raw URL:
+# https://gist.githubusercontent.com/jamiew/92937fc7b2a3e0192190b10c1cc8fa6b/raw/7c45ff90064e9b4f527260f52270b51361ead4d2/crypto-price-data.json
+set -e
+
+cd "$(dirname "$0")"
+
+gist_id="92937fc7b2a3e0192190b10c1cc8fa6b"
+filename="prices.json" # don't change! must match the gist 
+
+echo "Running, prettifying, and saving to $filename ..."
+echo "cwd=$(pwd -P)"
+/home/jamie/dev/crypto-price-data/run.sh | jq > "$filename"
+
+echo "Updating gist $gist_id ..."
+/usr/bin/gh gist edit "$gist_id" "$filename"
+
+echo "All done"
+rm "$filename"
