59df5333aedb7709b09403444126dab5b74086dc
Jamie Dubs <jamie@jamiedubs.com>
2024-02-18 08:46:43 -0500

    Refactoring to just all be javascript. Simpler

--- coingecko-symbols.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-url="https://api.coingecko.com/api/v3/coins/list"
-curl -s $url
--- price-magic.js
+++ price-magic.js
@@ -1,27 +1,59 @@
-const fs = require('fs')
+const fs = require("fs");
 
+function writeFile(filename, data) {
+  const fs = require("fs");
+  return fs.writeFileSync(filename, JSON.stringify(data));
+}
+
+function readConfig() {
+  const filename = "symbols.txt";
+  const data = fs.readFileSync(filename, "utf8");
+  if (!data || data.length == 0) {
+    throw new Error(`${filename} config file is blank`);
+  }
+  return data.split("\n");
+}
+
+async function fetchSymbols() {
+  return fetch("https://api.coingecko.com/api/v3/coins/list")
+    .then((response) => response.json())
+    .then((data) => {
+      writeFile("data2/symbols.json", data);
+      return data;
+    })
+    .catch((error) => console.error(error));
+}
+
+async function fetchPrices() {
+  const ids = readConfig().join(",");
+  const url = `https://api.coingecko.com/api/v3/simple/price?ids=${ids}&vs_currencies=usd`;
+  return fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      writeFile("data2/prices.json", data);
+      return data;
+    })
+    .catch((error) => console.error(error));
+}
 
 async function run() {
-  const symbols = JSON.parse(
-    await fs.readFileSync("data/symbols.json").toString()
-  );
+  const symbols = await fetchSymbols();
+  if (!symbols) throw new Error("no symbols");
+  const prices = await fetchPrices();
+  if (!prices) throw new Error("no prices");
 
-  const prices = JSON.parse(
-    await fs.readFileSync("data/prices.json").toString()
-  );
-
-  const newPrices = {},
-        justPrices = {};
-  for(let id in prices) {
-
+  const newPrices = {};
+  const justPrices = {};
+  for (let id in prices) {
     const usd = prices[id].usd;
+    // console.log(`id ${id} => ${usd}`);
 
     const res = symbols.filter((j) => {
-      if(j.id == id) return j;
+      if (j.id == id) return j;
     })[0];
 
     if (!res) {
-      // console.error(`error: missing data for ${id}`);
+      console.error(`error: missing data for ${id}`);
       continue;
     }
 
@@ -30,10 +62,11 @@ async function run() {
       name: res.name,
       symbol: res.symbol,
       usd: usd,
-    }
+    };
 
     justPrices[res.symbol] = usd;
   }
+
   // console.log(newPrices);
   // console.log(JSON.stringify(newPrices));
   console.log(JSON.stringify(justPrices));
