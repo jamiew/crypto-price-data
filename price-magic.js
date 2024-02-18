65c0080efd3c4d934ba1a2d875d9318ffd515f6e
Jamie Dubs <jamie@jamiedubs.com>
2024-02-18 08:47:15 -0500

    Refactor all js, simplify, try/catch

--- price-magic.js
+++ price-magic.js
@@ -1,78 +1,58 @@
 const fs = require("fs");
 
-function writeFile(filename, data) {
-  const fs = require("fs");
-  return fs.writeFileSync(filename, JSON.stringify(data));
-}
+const writeFile = (filename, data) =>
+  fs.writeFileSync(filename, JSON.stringify(data));
 
-function readConfig() {
+const readConfig = () => {
   const filename = "symbols.txt";
   const data = fs.readFileSync(filename, "utf8");
-  if (!data || data.length == 0) {
-    throw new Error(`${filename} config file is blank`);
-  }
+  if (!data) throw new Error(`${filename} config file is blank`);
   return data.split("\n");
-}
+};
 
-async function fetchSymbols() {
-  return fetch("https://api.coingecko.com/api/v3/coins/list")
-    .then((response) => response.json())
-    .then((data) => {
-      writeFile("data2/symbols.json", data);
-      return data;
-    })
-    .catch((error) => console.error(error));
-}
+const fetchJson = (url) => fetch(url).then((response) => response.json());
 
-async function fetchPrices() {
-  const ids = readConfig().join(",");
-  const url = `https://api.coingecko.com/api/v3/simple/price?ids=${ids}&vs_currencies=usd`;
-  return fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      writeFile("data2/prices.json", data);
-      return data;
-    })
-    .catch((error) => console.error(error));
-}
+const fetchSymbols = async () => {
+  try {
+    const data = await fetchJson("https://api.coingecko.com/api/v3/coins/list");
+    writeFile("data/symbols.json", data);
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-async function run() {
-  const symbols = await fetchSymbols();
-  if (!symbols) throw new Error("no symbols");
-  const prices = await fetchPrices();
-  if (!prices) throw new Error("no prices");
+const fetchPrices = async () => {
+  try {
+    const ids = readConfig().join(",");
+    const data = await fetchJson(
+      `https://api.coingecko.com/api/v3/simple/price?ids=${ids}&vs_currencies=usd`
+    );
+    writeFile("data/prices.json", data);
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const run = async () => {
+  const symbols = (await fetchSymbols()) || [];
+  const prices = (await fetchPrices()) || {};
 
   const newPrices = {};
-  const justPrices = {};
-  for (let id in prices) {
-    const usd = prices[id].usd;
-    // console.log(`id ${id} => ${usd}`);
-
-    const res = symbols.filter((j) => {
-      if (j.id == id) return j;
-    })[0];
-
-    if (!res) {
+  Object.entries(prices).forEach(([id, { usd }]) => {
+    const symbolData = symbols.find((j) => j.id === id);
+    if (!symbolData) {
       console.error(`error: missing data for ${id}`);
-      continue;
+      return;
     }
+    newPrices[symbolData.symbol] = usd;
+  });
 
-    newPrices[id] = {
-      // id: id,
-      name: res.name,
-      symbol: res.symbol,
-      usd: usd,
-    };
+  console.log(JSON.stringify(newPrices));
+};
 
-    justPrices[res.symbol] = usd;
-  }
-
-  // console.log(newPrices);
-  // console.log(JSON.stringify(newPrices));
-  console.log(JSON.stringify(justPrices));
-}
-
-run().catch((e) => {
-  console.error(e);
+run().catch((error) => {
+  console.error(error);
   process.exit(1);
 });
